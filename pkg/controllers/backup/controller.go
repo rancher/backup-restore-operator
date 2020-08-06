4624be38879b8799257d8ea0e6dd4ac228f4860b
rajashree <rajashree.28m@gmail.com>
2020-08-05 18:56:22 -0700

    Filters AND OR separate logic

--- pkg/controllers/backup/controller.go
+++ pkg/controllers/backup/controller.go
@@ -5,18 +5,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/rancher/wrangler/pkg/condition"
-	"github.com/rancher/wrangler/pkg/slice"
-	"github.com/sirupsen/logrus"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"regexp"
-	"strings"
-
 	v1 "github.com/mrajashree/backup/pkg/apis/backupper.cattle.io/v1"
 	util "github.com/mrajashree/backup/pkg/controllers"
 	backupControllers "github.com/mrajashree/backup/pkg/generated/controllers/backupper.cattle.io/v1"
+	"github.com/rancher/wrangler/pkg/condition"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -24,6 +18,9 @@ import (
 	"k8s.io/apiserver/pkg/storage/value"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 type handler struct {
@@ -34,8 +31,6 @@ type handler struct {
 	discoveryClient         discovery.DiscoveryInterface
 	dynamicClient           dynamic.Interface
 }
-
-var avoidBackupResources = map[string]bool{"pods": true}
 
 func Register(
 	ctx context.Context,
@@ -59,6 +54,9 @@ func Register(
 }
 
 func (h *handler) OnBackupChange(_ string, backup *v1.Backup) (*v1.Backup, error) {
+	if backup.DeletionTimestamp != nil || backup == nil {
+		return backup, nil
+	}
 	if condition.Cond(v1.BackupConditionReady).IsTrue(backup) && condition.Cond(v1.BackupConditionUploaded).IsTrue(backup) {
 		return backup, nil
 	}
@@ -94,6 +92,8 @@ func (h *handler) OnBackupChange(_ string, backup *v1.Backup) (*v1.Backup, error
 		}
 		return backup, err
 	}
+	resourceCollectionStartTime := time.Now()
+	logrus.Infof("Started gathering resources at %v", resourceCollectionStartTime)
 	err = h.gatherResources(template.BackupFilters, tmpBackupPath, transformerMap)
 	if err != nil {
 		removeDirErr := os.RemoveAll(tmpBackupPath)
@@ -102,6 +102,8 @@ func (h *handler) OnBackupChange(_ string, backup *v1.Backup) (*v1.Backup, error
 		}
 		return backup, err
 	}
+	timeTakenToCollectResources := time.Since(resourceCollectionStartTime)
+	logrus.Infof("time taken to collect resources: %v", timeTakenToCollectResources)
 	filters, err := json.Marshal(template.BackupFilters)
 	if err != nil {
 		removeDirErr := os.RemoveAll(tmpBackupPath)
@@ -159,164 +161,6 @@ func (h *handler) OnBackupChange(_ string, backup *v1.Backup) (*v1.Backup, error
 	return backup, err
 }
 
-func (h *handler) gatherResources(filters []v1.BackupFilter, backupPath string, transformerMap map[schema.GroupResource]value.Transformer) error {
-	for ind, filter := range filters {
-		resourceList, err := h.gatherResourcesForGroupVersion(filter)
-		if err != nil {
-			return err
-		}
-		if len(filter.Kinds) == 0 {
-			// user gave kinds in regex, updated kinds as a list of resource names that exactly matched the regex
-			for _, res := range resourceList {
-				if strings.Contains(res.Name, "/status") {
-					// example "customresourcedefinitions/status"
-					continue
-				}
-				filter.Kinds = append(filter.Kinds, res.Name)
-			}
-			filters[ind] = filter
-		}
-		gv, err := schema.ParseGroupVersion(filter.ApiGroup)
-		if err != nil {
-			return err
-		}
-
-		for _, res := range resourceList {
-			if skipBackup(res) {
-				continue
-			}
-			err := h.gatherObjectsForResource(res, gv, filter, backupPath, transformerMap)
-			if err != nil {
-				//fmt.Printf("\nerr in gatherObjectsForResource: %v\n", err)
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func (h *handler) gatherResourcesForGroupVersion(filter v1.BackupFilter) ([]k8sv1.APIResource, error) {
-	var resourceList []k8sv1.APIResource
-	//var resources []*k8sv1.APIResourceList
-	//var err error
-	groupVersion := filter.ApiGroup
-
-	//if groupVersion == "*" {
-	//	_, resources, err = h.discoveryClient.ServerGroupsAndResources()
-	//	if err != nil {
-	//		return resourceList, err
-	//	}
-	//}
-	resources, err := h.discoveryClient.ServerResourcesForGroupVersion(groupVersion)
-	if err != nil {
-		return resourceList, err
-	}
-
-	// resources has all resources under given groupVersion, only user the ones in filter.Kinds
-	if filter.KindsRegex == "." {
-		// continue to retrieve everything
-		resourceList = resources.APIResources
-	} else {
-		// else filter out resource with regex match
-		for _, res := range resources.APIResources {
-			matched, err := regexp.MatchString(filter.KindsRegex, res.Name)
-			if err != nil {
-				return resourceList, err
-			}
-			if !matched {
-				continue
-			}
-			logrus.Infof("resource kind %v matched regex %v\n", res.Name, filter.KindsRegex)
-			resourceList = append(resourceList, res)
-		}
-	}
-
-	// add the resources from Kinds field to this list
-	if len(filter.Kinds) > 0 {
-		resourceTypesToRetrieve := make(map[string]bool)
-		for _, res := range resourceList {
-			resourceTypesToRetrieve[res.Name] = true
-		}
-		for _, res := range resources.APIResources {
-			if slice.ContainsString(filter.Kinds, res.Name) && !resourceTypesToRetrieve[res.Name] {
-				resourceList = append(resourceList, res)
-			}
-		}
-	}
-	return resourceList, nil
-}
-
-func (h *handler) gatherObjectsForResource(res k8sv1.APIResource, gv schema.GroupVersion, filter v1.BackupFilter, backupPath string, transformerMap map[schema.GroupResource]value.Transformer) error {
-	var fieldSelector string
-	var filteredObjects []unstructured.Unstructured
-	gvr := gv.WithResource(res.Name)
-	var dr dynamic.ResourceInterface
-	dr = h.dynamicClient.Resource(gvr)
-
-	// if resource names or namespaces are provided, first filter using those as selectors
-	if len(filter.ResourceNames) > 0 || len(filter.Namespaces) > 0 {
-		for _, ns := range filter.ResourceNames {
-			fieldSelector += fmt.Sprintf("metadata.name=%s,", ns)
-		}
-		for _, ns := range filter.Namespaces {
-			fieldSelector += fmt.Sprintf("metadata.namespace=%s,", ns)
-		}
-
-		strings.TrimRight(fieldSelector, ",")
-		filteredObjectsList, err := dr.List(h.ctx, k8sv1.ListOptions{FieldSelector: fieldSelector})
-		if err != nil {
-			return err
-		}
-		filteredObjects = filteredObjectsList.Items
-	}
-
-	// resourceVersion - if unset, Return data at the most recent resource version. The returned data must be consistent (i.e. served from etcd via a quorum read).
-	resObjects, err := dr.List(h.ctx, k8sv1.ListOptions{FieldSelector: fieldSelector})
-	if err != nil {
-		return err
-	}
-	// check for regex
-	if filter.ResourceNameRegex != "" {
-		for _, resObj := range resObjects.Items {
-			metadata := resObj.Object["metadata"].(map[string]interface{})
-			name := metadata["name"].(string)
-			nameMatched, err := regexp.MatchString(filter.ResourceNameRegex, name)
-			if err != nil {
-				return err
-			}
-			if !nameMatched {
-				continue
-			}
-			filteredObjects = append(filteredObjects, resObj)
-		}
-	}
-	var filteredNs []string
-	if filter.NamespaceRegex != "" {
-		for _, resObj := range resObjects.Items {
-			metadata := resObj.Object["metadata"].(map[string]interface{})
-			namespace := metadata["namespace"].(string)
-			nsMatched, err := regexp.MatchString(filter.NamespaceRegex, namespace)
-			if err != nil {
-				return err
-			}
-			if !nsMatched {
-				continue
-			}
-			filteredObjects = append(filteredObjects, resObj)
-			filteredNs = append(filteredNs, namespace)
-		}
-	}
-	if res.Namespaced && len(filteredNs) > 0 {
-		filter.Namespaces = append(filter.Namespaces, filteredNs...)
-	}
-	if filter.NamespaceRegex == "" && filter.ResourceNameRegex == "" {
-		// no regex, return all objects
-		filteredObjects = resObjects.Items
-	}
-
-	return h.writeBackupObjects(filteredObjects, res, gv, backupPath, transformerMap)
-}
-
 func (h *handler) writeBackupObjects(resObjects []unstructured.Unstructured, res k8sv1.APIResource, gv schema.GroupVersion, backupPath string, transformerMap map[schema.GroupResource]value.Transformer) error {
 	for _, resObj := range resObjects {
 		metadata := resObj.Object["metadata"].(map[string]interface{})
@@ -341,7 +185,9 @@ func (h *handler) writeBackupObjects(resObjects []unstructured.Unstructured, res
 			}
 		}
 
-		for _, field := range []string{"uid", "generation", "creationTimestamp", "selfLink", "resourceVersion"} {
+		// TODO: decide whether to store deletionTimestamp or not
+		// TOTO:generation
+		for _, field := range []string{"uid", "creationTimestamp", "selfLink", "resourceVersion"} {
 			delete(metadata, field)
 		}
 
@@ -366,9 +212,6 @@ func (h *handler) writeBackupObjects(resObjects []unstructured.Unstructured, res
 }
 
 func skipBackup(res k8sv1.APIResource) bool {
-	if avoidBackupResources[res.Name] {
-		return true
-	}
 	if !canListResource(res.Verbs) {
 		logrus.Debugf("Cannot list resource %v, not backing up", res)
 		return true
--- pkg/controllers/backup/resourceCollector.go
+++ pkg/controllers/backup/resourceCollector.go
@@ -1,1 +1,254 @@
 package backup
+
+import (
+	"fmt"
+	v1 "github.com/mrajashree/backup/pkg/apis/backupper.cattle.io/v1"
+	"github.com/sirupsen/logrus"
+	k8sv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apiserver/pkg/storage/value"
+	"k8s.io/client-go/dynamic"
+	"regexp"
+	"strings"
+)
+
+func (h *handler) gatherResources(filters []v1.BackupFilter, backupPath string, transformerMap map[schema.GroupResource]value.Transformer) error {
+	for _, filter := range filters {
+		resourceList, err := h.gatherResourcesForGroupVersion(filter)
+		if err != nil {
+			return err
+		}
+
+		gv, err := schema.ParseGroupVersion(filter.ApiGroup)
+		if err != nil {
+			return err
+		}
+
+		for _, res := range resourceList {
+			if skipBackup(res) {
+				continue
+			}
+			err := h.gatherAndWriteObjectsForResource(res, gv, filter, backupPath, transformerMap)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (h *handler) gatherResourcesForGroupVersion(filter v1.BackupFilter) ([]k8sv1.APIResource, error) {
+	var resourceList, resourceListFromRegex, resourceListFromNames []k8sv1.APIResource
+	groupVersion := filter.ApiGroup
+
+	// TODO: accept all groupVersions in one filter
+	//if groupVersion == "*" {
+	//	_, resources, err = h.discoveryClient.ServerGroupsAndResources()
+	//	if err != nil {
+	//		return resourceList, err
+	//	}
+	//}
+	resources, err := h.discoveryClient.ServerResourcesForGroupVersion(groupVersion)
+	if err != nil {
+		return resourceList, err
+	}
+
+	// resources list has all resources under given groupVersion, first filter based on KindsRegex
+	if filter.KindsRegex != "" {
+		if filter.KindsRegex == "." {
+			// continue to retrieve everything
+			return resources.APIResources, nil
+		}
+		// else filter out resource with regex match
+		for _, res := range resources.APIResources {
+			matched, err := regexp.MatchString(filter.KindsRegex, res.Name)
+			if err != nil {
+				return resourceList, err
+			}
+			if !matched {
+				continue
+			}
+			logrus.Infof("resource kind %v matched regex %v\n", res.Name, filter.KindsRegex)
+			resourceListFromRegex = append(resourceListFromRegex, res)
+		}
+	}
+
+	// then add the resources from Kinds field to this list
+	if len(filter.Kinds) > 0 {
+		resourceListAfterRegexMatch := make(map[string]bool)
+		for _, res := range resourceListFromRegex {
+			resourceListAfterRegexMatch[res.Name] = true
+		}
+		resourceTypesToInclude := make(map[string]bool)
+		for _, kind := range filter.Kinds {
+			resourceTypesToInclude[kind] = true
+		}
+		for _, res := range resources.APIResources {
+			if resourceTypesToInclude[res.Name] && !resourceListAfterRegexMatch[res.Name] {
+				resourceListFromNames = append(resourceListFromNames, res)
+			}
+		}
+	}
+	// combine both
+	resourceList = append(resourceListFromRegex, resourceListFromNames...)
+	return resourceList, nil
+}
+
+func (h *handler) gatherAndWriteObjectsForResource(res k8sv1.APIResource, gv schema.GroupVersion, filter v1.BackupFilter, backupPath string, transformerMap map[schema.GroupResource]value.Transformer) error {
+	var filteredByNamespace, filteredObjects []unstructured.Unstructured
+
+	gvr := gv.WithResource(res.Name)
+	var dr dynamic.ResourceInterface
+	dr = h.dynamicClient.Resource(gvr)
+
+	filteredByName, err := h.filterByNameAndLabel(dr, filter)
+	if err != nil {
+		return err
+	}
+
+	if res.Namespaced {
+		if len(filter.Namespaces) > 0 || filter.NamespaceRegex != "" {
+			filteredByNamespace, err = h.filterByNamespace(filter, filteredByName)
+			if err != nil {
+				return err
+			}
+			filteredObjects = filteredByNamespace
+			return h.writeBackupObjects(filteredObjects, res, gv, backupPath, transformerMap)
+		}
+	}
+	filteredObjects = filteredByName
+	return h.writeBackupObjects(filteredObjects, res, gv, backupPath, transformerMap)
+}
+
+func (h *handler) filterByNameAndLabel(dr dynamic.ResourceInterface, filter v1.BackupFilter) ([]unstructured.Unstructured, error) {
+	var filteredByName, filteredByResourceNames []unstructured.Unstructured
+	var fieldSelector, labelSelector string
+	// first get all objects of this resource type
+	if filter.LabelSelectors != nil {
+		labelMap, err := k8sv1.LabelSelectorAsMap(filter.LabelSelectors)
+		if err != nil {
+			return filteredByName, err
+		}
+		labelSelector = labels.SelectorFromSet(labelMap).String()
+	}
+
+	resourceObjectsList, err := dr.List(h.ctx, k8sv1.ListOptions{LabelSelector: labelSelector})
+	// TODO: get resourceVersion and do another call for List same for all resources; check err for resourceVersion
+	if err != nil {
+		return filteredByName, err
+	}
+	filteredByNameMap := make(map[*unstructured.Unstructured]bool)
+
+	if len(filter.ResourceNames) == 0 && filter.ResourceNameRegex == "" {
+		// no filters for names of the resource, return all objects from list
+		return resourceObjectsList.Items, nil
+	}
+	// filter out using ResourceNameRegex
+	if filter.ResourceNameRegex != "" {
+		if filter.ResourceNameRegex == "." {
+			// include all resources obtained from list
+			return resourceObjectsList.Items, nil
+		}
+
+		for _, resObj := range resourceObjectsList.Items {
+			metadata := resObj.Object["metadata"].(map[string]interface{})
+			name := metadata["name"].(string)
+			nameMatched, err := regexp.MatchString(filter.ResourceNameRegex, name)
+			if err != nil {
+				return filteredByName, err
+			}
+			if !nameMatched {
+				continue
+			}
+			filteredByName = append(filteredByName, resObj)
+			filteredByNameMap[&resObj] = true
+		}
+	}
+
+	// filter by names as fieldSelector:
+	if len(filter.ResourceNames) > 0 {
+		for _, name := range filter.ResourceNames {
+			fieldSelector += fmt.Sprintf("metadata.name=%s,", name)
+		}
+		strings.TrimRight(fieldSelector, ",")
+		filteredObjectsList, err := dr.List(h.ctx, k8sv1.ListOptions{FieldSelector: fieldSelector, LabelSelector: labelSelector})
+		if err != nil {
+			return filteredByName, err
+		}
+		filteredByResourceNames = filteredObjectsList.Items
+
+		if len(filteredByResourceNames) == 0 {
+			// none matched
+			return filteredByName, nil
+		}
+		if len(filteredByNameMap) > 0 {
+			for _, resObj := range filteredByResourceNames {
+				if !filteredByNameMap[&resObj] {
+					filteredByName = append(filteredByName, resObj)
+				}
+			}
+		} else {
+			filteredByName = filteredByResourceNames
+		}
+
+	}
+	return filteredByName, nil
+}
+
+func (h *handler) filterByNamespace(filter v1.BackupFilter, filteredByName []unstructured.Unstructured) ([]unstructured.Unstructured, error) {
+	var filteredByNamespace, filteredByNamespaceRegex, filteredObjects []unstructured.Unstructured
+
+	filteredByNsMap := make(map[*unstructured.Unstructured]bool)
+
+	if len(filter.Namespaces) > 0 {
+		allowedNamespaces := make(map[string]bool)
+		for _, ns := range filter.Namespaces {
+			allowedNamespaces[ns] = true
+		}
+		for _, resObj := range filteredByName {
+			metadata := resObj.Object["metadata"].(map[string]interface{})
+			ns := metadata["namespace"].(string)
+			if allowedNamespaces[ns] {
+				filteredByNamespace = append(filteredByNamespace, resObj)
+				filteredByNsMap[&resObj] = true
+			}
+		}
+	}
+	if filter.NamespaceRegex != "" {
+		if filter.NamespaceRegex == "." {
+			// include all namespaces, so return all objects obtained after filtering by name
+			return filteredByName, nil
+		}
+		for _, resObj := range filteredByName {
+			metadata := resObj.Object["metadata"].(map[string]interface{})
+			ns := metadata["namespace"].(string)
+			nsMatched, err := regexp.MatchString(filter.NamespaceRegex, ns)
+			if err != nil {
+				return filteredByNamespace, err
+			}
+			if !nsMatched {
+				continue
+			}
+			filteredByNamespaceRegex = append(filteredByNamespaceRegex, resObj)
+		}
+
+		if len(filteredByNamespaceRegex) == 0 {
+			// none matched regex
+			return filteredByNamespace, nil
+		}
+
+		if len(filteredByNsMap) > 0 {
+			for _, resObj := range filteredByNamespaceRegex {
+				if !filteredByNsMap[&resObj] {
+					filteredByNamespace = append(filteredByNamespace, resObj)
+				}
+			}
+		} else {
+			filteredByNamespace = filteredByNamespaceRegex
+		}
+	}
+	filteredObjects = append(filteredByNamespace, filteredByNamespaceRegex...)
+	return filteredObjects, nil
+}
