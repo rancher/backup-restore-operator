37ba5b2be8284570886cedf007229a3c074d2525
Jiaqi Luo <6218999+jiaqiluo@users.noreply.github.com>
2021-11-17 15:06:12 -0700

    run go generate

--- pkg/generated/controllers/resources.cattle.io/v1/backup.go
+++ pkg/generated/controllers/resources.cattle.io/v1/backup.go
@@ -318,11 +318,19 @@ func (a *backupStatusHandler) sync(key string, obj *v1.Backup) (*v1.Backup, erro
 		}
 	}
 	if !equality.Semantic.DeepEqual(origStatus, &newStatus) {
+		if a.condition != "" {
+			// Since status has changed, update the lastUpdatedTime
+			a.condition.LastUpdated(&newStatus, time.Now().UTC().Format(time.RFC3339))
+		}
+
 		var newErr error
 		obj.Status = newStatus
-		obj, newErr = a.client.UpdateStatus(obj)
+		newObj, newErr := a.client.UpdateStatus(obj)
 		if err == nil {
 			err = newErr
+		}
+		if newErr == nil {
+			obj = newObj
 		}
 	}
 	return obj, err
@@ -352,6 +360,10 @@ func (a *backupGeneratingHandler) Remove(key string, obj *v1.Backup) (*v1.Backup
 }
 
 func (a *backupGeneratingHandler) Handle(obj *v1.Backup, status v1.BackupStatus) (v1.BackupStatus, error) {
+	if !obj.DeletionTimestamp.IsZero() {
+		return status, nil
+	}
+
 	objs, newStatus, err := a.BackupGeneratingHandler(obj, status)
 	if err != nil {
 		return newStatus, err
--- pkg/generated/controllers/resources.cattle.io/v1/restore.go
+++ pkg/generated/controllers/resources.cattle.io/v1/restore.go
@@ -318,11 +318,19 @@ func (a *restoreStatusHandler) sync(key string, obj *v1.Restore) (*v1.Restore, e
 		}
 	}
 	if !equality.Semantic.DeepEqual(origStatus, &newStatus) {
+		if a.condition != "" {
+			// Since status has changed, update the lastUpdatedTime
+			a.condition.LastUpdated(&newStatus, time.Now().UTC().Format(time.RFC3339))
+		}
+
 		var newErr error
 		obj.Status = newStatus
-		obj, newErr = a.client.UpdateStatus(obj)
+		newObj, newErr := a.client.UpdateStatus(obj)
 		if err == nil {
 			err = newErr
+		}
+		if newErr == nil {
+			obj = newObj
 		}
 	}
 	return obj, err
@@ -352,6 +360,10 @@ func (a *restoreGeneratingHandler) Remove(key string, obj *v1.Restore) (*v1.Rest
 }
 
 func (a *restoreGeneratingHandler) Handle(obj *v1.Restore, status v1.RestoreStatus) (v1.RestoreStatus, error) {
+	if !obj.DeletionTimestamp.IsZero() {
+		return status, nil
+	}
+
 	objs, newStatus, err := a.RestoreGeneratingHandler(obj, status)
 	if err != nil {
 		return newStatus, err
