9c6294038c35d3e67e0b31a222f714675a217956
Dan Pock <self@danpock.me>
2024-04-10 09:24:18 -0400

    adjust variable name to correct overlap with package of same name.

--- pkg/crds/crd.go
+++ pkg/crds/crd.go
@@ -23,25 +23,25 @@ func WriteCRD() error {
 			return err
 		}
 		newObj, _ := bCrd.(*unstructured.Unstructured)
-		var crd apiext.CustomResourceDefinition
-		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(newObj.Object, &crd); err != nil {
+		var crdItem apiext.CustomResourceDefinition
+		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(newObj.Object, &crdItem); err != nil {
 			return err
 		}
-		switch crd.Name {
+		switch crdItem.Name {
 		case "backups.resources.cattle.io":
-			customizeBackup(&crd)
+			customizeBackup(&crdItem)
 		case "resourcesets.resources.cattle.io":
-			customizeResourceSet(&crd)
+			customizeResourceSet(&crdItem)
 		case "restores.resources.cattle.io":
-			customizeRestore(&crd)
+			customizeRestore(&crdItem)
 		}
 
-		yamlBytes, err := yaml.Export(&crd)
+		yamlBytes, err := yaml.Export(&crdItem)
 		if err != nil {
 			return err
 		}
 
-		filename := fmt.Sprintf("./charts/rancher-backup-crd/templates/%s.yaml", strings.ToLower(crd.Spec.Names.Kind))
+		filename := fmt.Sprintf("./charts/rancher-backup-crd/templates/%s.yaml", strings.ToLower(crdItem.Spec.Names.Kind))
 		err = ioutil.WriteFile(filename, yamlBytes, 0644)
 		if err != nil {
 			return err
@@ -128,7 +128,7 @@ func customizeRestore(restore *apiext.CustomResourceDefinition) {
 }
 
 func newCRD(obj interface{}, customize func(crd.CRD) crd.CRD) crd.CRD {
-	crd := crd.CRD{
+	crdItem := crd.CRD{
 		GVK: schema.GroupVersionKind{
 			Group:   "resources.cattle.io",
 			Version: "v1",
@@ -138,7 +138,7 @@ func newCRD(obj interface{}, customize func(crd.CRD) crd.CRD) crd.CRD {
 		NonNamespace: true,
 	}
 	if customize != nil {
-		crd = customize(crd)
+		crdItem = customize(crdItem)
 	}
-	return crd
+	return crdItem
 }
