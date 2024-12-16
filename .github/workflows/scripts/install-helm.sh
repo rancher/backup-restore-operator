0d73e5113ebfbd5489909e6988aca236d701829f
Alexandre Lamarre <alexandre.lamarre@suse.com>
2024-12-16 11:10:57 -0500

    remove Dapper
    Signed-off-by: Alexandre Lamarre <alexandre.lamarre@suse.com>

---
+++ .github/workflows/scripts/install-helm.sh
@@ -0,0 +1,5 @@
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+helm plugin install https://github.com/quintush/helm-unittest
