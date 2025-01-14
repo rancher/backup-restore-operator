fc36a9b4e0f1590780e2ce42916b766d03eb1131
Dan Pock <self@danpock.me>
2025-01-14 16:27:33 -0500

    Additional refactor for testability

--- pkg/util/encryptionconfig/config.go
+++ pkg/util/encryptionconfig/config.go
@@ -16,9 +16,10 @@ import (
 	"k8s.io/apiserver/pkg/storage/value/encrypt/identity"
 )
 
+const contextKey = "tmpConfigPath"
 const EncryptionProviderConfigKey = "encryption-provider-config.yaml"
 
-func GetEncryptionTransformersFromSecret(ctx context.Context, encryptionConfigSecretName string, secrets v1.SecretController) (k8sEncryptionconfig.StaticTransformers, error) {
+func GetEncryptionConfigSecret(secrets v1.SecretController, encryptionConfigSecretName string) (*v1core.Secret, error) {
 	// EncryptionConfig secret ns is hardcoded to ns of controller in chart's ns
 	// kubectl create secret generic test-encryptionconfig --from-file=./encryption-provider-config.yaml
 	logrus.Infof("Get encryption config from namespace %v", util.GetChartNamespace())
@@ -26,25 +27,38 @@ func GetEncryptionTransformersFromSecret(ctx context.Context, encryptionConfigSe
 	if err != nil {
 		return nil, err
 	}
-	err = PrepareEncryptionConfigSecretTempConfig(encryptionConfigSecret)
+
+	return encryptionConfigSecret, nil
+}
+
+func GetEncryptionTransformersFromSecret(ctx context.Context, encryptionConfigSecret *v1core.Secret) (k8sEncryptionconfig.StaticTransformers, error) {
+	fileHandle, err := PrepareEncryptionConfigSecretTempConfig(encryptionConfigSecret)
+	// we defer file removal till here to ensure it's around for all of PrepareEncryptionTransformersFromConfig
+	defer os.Remove(EncryptionProviderConfigKey)
 	if err != nil {
 		return nil, err
 	}
+	ctx = context.WithValue(ctx, contextKey, fileHandle.Name())
 	return PrepareEncryptionTransformersFromConfig(ctx, EncryptionProviderConfigKey)
 }
 
-func PrepareEncryptionConfigSecretTempConfig(encryptionConfigSecret *v1core.Secret) error {
+func PrepareEncryptionConfigSecretTempConfig(encryptionConfigSecret *v1core.Secret) (*os.File, error) {
 	encryptionConfigBytes, ok := encryptionConfigSecret.Data[EncryptionProviderConfigKey]
 	if !ok {
-		return fmt.Errorf("no encryptionConfig provided")
+		return nil, fmt.Errorf("no encryptionConfig provided")
 	}
 	err := os.WriteFile(EncryptionProviderConfigKey, encryptionConfigBytes, os.ModePerm)
-	defer os.Remove(EncryptionProviderConfigKey)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	// Open the file for reading (or other operations) and return the handle
+	file, err := os.Open(EncryptionProviderConfigKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
 }
 
 func PrepareEncryptionTransformersFromConfig(ctx context.Context, encryptionProviderPath string) (k8sEncryptionconfig.StaticTransformers, error) {
--- pkg/util/encryptionconfig/config_test.go
+++ pkg/util/encryptionconfig/config_test.go
@@ -2,7 +2,10 @@ package encryptionconfig
 
 import (
 	"context"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -20,6 +23,74 @@ var deploymentGVR = schema.GroupVersionResource{
 	Group:    "apps",
 	Version:  "v1",
 	Resource: "deployments",
+}
+
+func TestGetEncryptionTransformersFromSecret_Basic(t *testing.T) {
+	encryptionConfigFilepath := filepath.Join("testdata", "encryption-provider-config-wildcard.yaml")
+	configBytes, _ := os.ReadFile(encryptionConfigFilepath)
+
+	testSecret := v1.Secret{
+		TypeMeta:   metav1.TypeMeta{APIVersion: "v1", Kind: "Secret"},
+		ObjectMeta: metav1.ObjectMeta{Name: "test-secret"},
+		Data: map[string][]byte{
+			EncryptionProviderConfigKey: configBytes,
+		},
+	}
+	transformers, err := GetEncryptionTransformersFromSecret(
+		context.Background(),
+		&testSecret,
+	)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, transformers)
+}
+
+func TestPrepareEncryptionConfigSecretTempConfig_ValidSecretKeySillyData(t *testing.T) {
+	sillyTestData := "Hello Rancher! Moo."
+	testSecret := v1.Secret{
+		TypeMeta:   metav1.TypeMeta{APIVersion: "v1", Kind: "Secret"},
+		ObjectMeta: metav1.ObjectMeta{Name: "test-secret"},
+		Data: map[string][]byte{
+			EncryptionProviderConfigKey: []byte(sillyTestData),
+		},
+	}
+	fileHandle, err := PrepareEncryptionConfigSecretTempConfig(&testSecret)
+	defer os.Remove(EncryptionProviderConfigKey)
+	// Assert that no error is returned
+	assert.Nil(t, err)
+
+	// Read the file written by PrepareEncryptionConfigSecretTempConfig
+	actualBytes := make([]byte, 1024)
+	n, err := fileHandle.Read(actualBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Assert that the contents of the file match the expected encryption config value
+	assert.Equal(t, sillyTestData, string(actualBytes[:n]))
+}
+
+func TestPrepareEncryptionConfigSecretTempConfig_EmptySecret(t *testing.T) {
+	testSecret := v1.Secret{}
+	_, err := PrepareEncryptionConfigSecretTempConfig(&testSecret)
+	assert.NotNil(t, err)
+	assert.Error(t, err)
+	assert.ErrorContains(t, err, "no encryptionConfig provided")
+}
+
+func TestPrepareEncryptionConfigSecretTempConfig_IncorrectSecretKey(t *testing.T) {
+	testSecret := v1.Secret{
+		TypeMeta:   metav1.TypeMeta{APIVersion: "v1", Kind: "Secret"},
+		ObjectMeta: metav1.ObjectMeta{Name: "test-secret"},
+		Data: map[string][]byte{
+			"key": []byte("value"),
+		},
+	}
+	_, err := PrepareEncryptionConfigSecretTempConfig(&testSecret)
+	assert.NotNil(t, err)
+	assert.Error(t, err)
+	assert.ErrorContains(t, err, "no encryptionConfig provided")
+
 }
 
 func TestPrepareEncryptionTransformersFromConfig_Wildcard(t *testing.T) {
@@ -56,7 +127,8 @@ func TestIsDefaultEncryptionTransformer_Wildcard(t *testing.T) {
 
 func TestIsDefaultEncryptionTransformer_PartialWildcard(t *testing.T) {
 	encryptionConfigFilepath := filepath.Join("testdata", "encryption-provider-config-partial-wildcard.yaml")
-	transformers, err := PrepareEncryptionTransformersFromConfig(context.Background(), encryptionConfigFilepath)
+	ctx := context.WithValue(context.Background(), contextKey, encryptionConfigFilepath)
+	transformers, err := PrepareEncryptionTransformersFromConfig(ctx, encryptionConfigFilepath)
 	assert.Nil(t, err)
 
 	serviceAccountTransformer := transformers.TransformerForResource(serviceAccountGVR.GroupResource())
