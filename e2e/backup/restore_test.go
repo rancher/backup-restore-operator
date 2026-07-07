0229d349f03ff8ad306bd234c9b99f272e0b7c6d
Dan Pock <self@danpock.me>
2026-07-07 14:58:39 -0400

    fix lints

--- e2e/backup/restore_test.go
+++ e2e/backup/restore_test.go
@@ -355,7 +355,6 @@ var _ = Describe("Restore from remote driver", Ordered, Label("integration"), fu
 		})
 	})
 
-
 	When("we're done with all test restores", func() {
 		Specify("we should eventually have the correct restore metadata metrics", func() {
 
